20bd64978c23d2cf4fce68bf0a04a6c3ead72b50
krishmalik007 <krishmalik2698@gmail.com>
2025-12-05 09:32:12 +0530

    commit

--- CLASS PROJECTS/CHANGE THE PROFILE WITH JAVASCRIPT CODE/profilejs.js
+++ CLASS PROJECTS/CHANGE THE PROFILE WITH JAVASCRIPT CODE/profilejs.js
@@ -1,4 +1,4 @@
-// Selectors
+
 const profileName = document.querySelector("#profile-name");
 const profileBio = document.querySelector("#profile-bio");
 const profilePic = document.querySelector("#profile-pic");
@@ -8,24 +8,24 @@ const bioInput = document.querySelector("#bio-input");
 const picInput = document.querySelector("#pic-input");
 const updateButton = document.querySelector("#update-button");
 
-// Event Listener for update button
+
 updateButton.addEventListener("click", () => {
-  // Update profile name
+  
   if (nameInput.value !== "") {
     profileName.innerText = nameInput.value;
   }
 
-  // Update profile bio
+  
   if (bioInput.value !== "") {
     profileBio.textContent = bioInput.value;
   }
 
-  // Update profile picture
+
   if (picInput.value !== "") {
     profilePic.setAttribute("src", picInput.value);
   }
 
-  // Clear inputs
+
   nameInput.value = "";
   bioInput.value = "";
   picInput.value = "";
